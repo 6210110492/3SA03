1dfbd4305557e53cb06d79e1067f377affea46d9
6210110492 <6210110492@psu.ac.th>
2021-07-12 01:45:21 +0700

    สร้างหมวดการแสดงผล

--- src/App.js
+++ src/App.js
@@ -5,16 +5,24 @@ import WordCard from './WordCard';
 
 
 function App() {
-  const word = ["Fruit","Apple","Banana","Coconut","Durian"]
-  const randomWord = word[Math.floor(Math.random()*word.length)];
+  const Fruit = ["Apple","Banana","Coconut","Durian"];
+  const Animal = ["Ant","Brid","Cat","Dog"];
+  const Vegetable = ["Acacia","Basil","Chilli","Dill"];
+  const randomFruit = Fruit[Math.floor(Math.random()*Fruit.length)];
+  const randomAnimal = Animal[Math.floor(Math.random()*Animal.length)];
+  const randomVegetable = Vegetable[Math.floor(Math.random()*Vegetable.length)];
   const newgame = () => {window.location.reload()}  
   return (    
       <div> 
         <header>
           <h1> React Game </h1>
         </header>
-
-        <WordCard value={randomWord}/>
+        <h2> Fruit </h2>
+        <WordCard value={randomFruit}/>
+        <h2> Animal </h2>
+        <WordCard value={randomAnimal}/>
+        <h2> Vegetable </h2>
+        <WordCard value={randomVegetable}/>
         <button onClick={newgame}>
           Restart
         </button>
